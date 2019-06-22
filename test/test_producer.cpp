dfc6684f68a641d27a46d07da031058643831290
Tomasz Galecki <saleph@protonmail.com>
2019-06-22 18:15:21 +0200

    futher cleanup - removed tests from cmake level during tidy

--- test/test_producer.cpp
+++ test/test_producer.cpp
@@ -1,5 +1,3 @@
-// unfortunately that's the only way to supppres gtests warnings
-#ifndef __clang_analyzer__
 #include <gtest/gtest.h>
 #include <producer.h>
 #include <iostream>
@@ -14,5 +12,3 @@ TEST(someshit, eloel2)
     producer p{};
     ASSERT_NO_THROW(p.print_msg());
 }
-
-#endif //__clang_analyzer__
