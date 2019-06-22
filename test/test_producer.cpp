5b81bb3c6ac4b9ea5ec7054c29326f182708d54c
Tomasz Galecki <saleph@protonmail.com>
2019-06-22 13:52:14 +0200

    fix warnings in gtests + travis cleanup for piping

--- test/test_producer.cpp
+++ test/test_producer.cpp
@@ -1,27 +1,5 @@
-//
-// CI Hello World
-//
-// Copyright (C) 2017 Assured Information Security, Inc.
-// Author: Rian Quinn        <quinnr@ainfosec.com>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
+// unfortunately that's the only way to supppres gtests warnings
+#ifndef __clang_analyzer__
 #include <gtest/gtest.h>
 #include <producer.h>
 #include <iostream>
@@ -36,3 +14,5 @@ TEST(someshit, eloel2)
     producer p{};
     ASSERT_NO_THROW(p.print_msg());
 }
+
+#endif //__clang_analyzer__
